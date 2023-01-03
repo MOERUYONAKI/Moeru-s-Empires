433ca9d28de18a1f77b51a697548980d16a90879
Moeru <105922034+MOERUYONAKI@users.noreply.github.com>
2023-01-03 09:36:33 +0100

    new collaborator

---
+++ Sleddge_modules/a_rename.py
@@ -0,0 +1,8 @@
+"""
+à faire :
+    1 - Un script python créant un empire composé de plusieurs royaumes possèdant eux-même plusieurs villes (voir empire.py)
+    2 - Un bot discord avec une commande "pong" qui renvoie "ping" avec les ms du bot
+    3 - Un bot discord qui réagit quand un message est posté (sur le même bot)
+    4 - Un bot discord qui renvoie l'empire créé dans un autre script python
+    5 - 
+"""
