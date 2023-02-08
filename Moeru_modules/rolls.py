80ca111965c5e77704a5afd6455beac257fda2f6
Moeru <105922034+MOERUYONAKI@users.noreply.github.com>
2023-02-08 15:44:46 +0100

    new roll : "basic_roll"
    Dice roll / probabilities equals for each values
    > Return a number upper than 0 and lower or equal than the "max"

--- Moeru_modules/rolls.py
+++ Moeru_modules/rolls.py
@@ -1,6 +1,15 @@
 #  > - <  I P O R T S  > - <  #
 
+
 import random as rdm 
 
+
 #  > - <  F O N C T I O N S  > - <  #
+
+
+def basic_roll(max : int):
+    '''Dice roll / probabilities equals for each values
+    > Return a number upper than 0 and lower or equal than the "max"'''
+    nbr = rdm.randint(0, max)
+    return nbr
 
